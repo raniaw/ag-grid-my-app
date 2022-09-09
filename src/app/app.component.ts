632ae0fbf55c250aa6c78dbd784e44945363539c
Wittenberg Rania <WITTER@de.softlab.net>
2022-09-09 20:16:32 +0200

    add new file for ag-grid-my-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CellClickedEvent, ColDef } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+/*   rowData: any[] = [
+    {make:'Toyota', model : 'Celica', price:35000},
+    {make:'Ford', model : 'Mondeo', price:32000},
+    {make:'Porsche', model : 'Boxter', price:72000}
+  ]; */
+  rowData$!: Observable<any[]>;
+  colDefs: ColDef[] = [
+    {field:'make'},
+    {field:'model'},
+    {field: 'price'}
+  ];
+
+  defaultColDef: ColDef = {
+    sortable:true, filter:true
+  }
+
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+  constructor(private http: HttpClient){}
+  ngOnInit(){
+    this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json')
+  }
+  onCellClicked(event:CellClickedEvent){
+    console.log(event)
+  }
+  clearSelection(){
+    this.agGrid.api.deselectAll();
+  }
 }
